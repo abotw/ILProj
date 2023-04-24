aff0c5f6be489bc02eb942ec34a317f10a6439e6
worldrise <341266076@qq.com>
2023-04-24 17:04:04 +0800

    test

---
+++ backend/backend/src/main/java/com/ilproj/backend/Test.java
@@ -0,0 +1,8 @@
+package com.ilproj.backend;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test1");
+        System.out.println("test2");
+    }
+}
