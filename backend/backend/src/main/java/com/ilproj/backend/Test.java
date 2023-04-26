72e5cd259d7c7740b15fa48ac1f198027d151ef2
worldrise <341266076@qq.com>
2023-04-26 13:33:22 +0800

    2023.4.26 text

--- backend/backend/src/main/java/com/ilproj/backend/Test.java
+++ backend/backend/src/main/java/com/ilproj/backend/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("2023.4.26 text");
     }
 }
