0524417a872ec130f88b567aa2e059c661c99c56
worldrise <341266076@qq.com>
2023-04-29 01:01:34 +0800

    增加注册接口和登录接口

---
+++ backend/backend/src/main/java/com/ilproj/sys/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.ilproj.sys.mapper;
+
+import com.ilproj.sys.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author IL
+ * @since 2023-04-27
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
